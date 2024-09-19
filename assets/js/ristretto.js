680eb32b450f3e1ef733dfaef462402c3ef7a119
Umair Afzal <128166400+umair-afzal-uat@users.noreply.github.com>
2024-09-19 08:31:12 -0700

    Fix: Update JS
    Update JS

--- assets/js/ristretto.js
+++ assets/js/ristretto.js
@@ -1,31 +1,28 @@
 function toggleCard(card) {
-    var cardBody = card.nextElementSibling;
-    var icon = card.querySelector(".rotate-icon");
+  const cardBody = card.nextElementSibling;
+  const icon = card.querySelector(".rotate-icon");
+  const mainCardClass = "active";
 
-    var mainCardClass = "active";
-
-    // Close all other cards
-    var allCards = document.querySelectorAll(".card-body");
-    allCards.forEach(function (otherCard) {
-      if (otherCard !== cardBody) {
-        otherCard.style.maxHeight = null;
-        otherCard.previousElementSibling
-          .querySelector(".rotate-icon")
-          .classList.remove("rotate");
-
-          otherCard.previousElementSibling.classList.remove(mainCardClass);
-      }
-    });
-    if (
-      cardBody.style.maxHeight === "0px" ||
-      cardBody.style.maxHeight === ""
-    ) {
-      cardBody.style.maxHeight = cardBody.scrollHeight + "px";
-      icon.classList.add("rotate");
-      card.classList.add(mainCardClass);
-    } else {
-      cardBody.style.maxHeight = null;
-      icon.classList.remove("rotate");
-      card.classList.remove(mainCardClass);
+  // Close all other cards
+  const allCards = document.querySelectorAll(".card-body");
+  allCards.forEach((otherCard) => {
+    if (otherCard !== cardBody) {
+      otherCard.style.maxHeight = null;
+      otherCard.previousElementSibling
+        .querySelector(".rotate-icon")
+        .classList.remove("rotate");
+      otherCard.previousElementSibling.classList.remove(mainCardClass);
     }
+  });
+
+  // Toggle the clicked card
+  if (cardBody.style.maxHeight === "0px" || cardBody.style.maxHeight === "") {
+    cardBody.style.maxHeight = `${cardBody.scrollHeight}px`;
+    icon.classList.add("rotate");
+    card.classList.add(mainCardClass);
+  } else {
+    cardBody.style.maxHeight = null;
+    icon.classList.remove("rotate");
+    card.classList.remove(mainCardClass);
   }
+}
